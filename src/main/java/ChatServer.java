537cf59a5e2302d3fe97e9ee8075df0b2ec83c3e
Ignat <lazovikignat@gmail.com>
2024-11-04 23:41:47 +0300

    Write Server prototype

--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -1,18 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import ClientMessage.AbstractClientMessage;
+import ServerMessages.ChatMessage;
+import ServerMessages.ResultMessage;
+
+import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.net.ServerSocket;
 
 public class ChatServer {
-    private static List<PrintWriter> clients;
+    private static List<ObjectOutputStream> clients;
+    private static Set<String> activeNicks;
     private static ServerSocket server;
 
     public ChatServer() {
-        clients = new ArrayList<PrintWriter>();
+        clients = new ArrayList<ObjectOutputStream>();
+        activeNicks = new HashSet<String>();
     }
 
     public void run() {
@@ -34,20 +36,65 @@ public class ChatServer {
 
     private static class ClientHandler extends Thread {
         private Socket sock;
-        private BufferedReader in;
-        private PrintWriter out;
+        private ObjectInputStream in;
+        private ObjectOutputStream out;
+        private boolean isRegistered;
+        private String nick;
 
         public ClientHandler(Socket sock) {
             this.sock = sock;
+            this.isRegistered = false;
+        }
+
+        private void sendResult(int code){
+            try{
+                out.writeObject(new ResultMessage(code));
+            }
+            catch(IOException e){}
         }
 
         public void run() {
             try {
-                in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-                out = new PrintWriter(sock.getOutputStream(), true);
-                clients.add(out);
+                in = new ObjectInputStream(sock.getInputStream());
+                out = new ObjectOutputStream(sock.getOutputStream());
+                //clients.add(out);
             } catch (IOException e) {
                 throw new RuntimeException(e);
+            }
+
+            AbstractClientMessage clientMessage;
+            while(!isRegistered && sock.isConnected()) {
+                try{
+                    clientMessage = (AbstractClientMessage)(in.readObject());
+                    if(activeNicks.contains(clientMessage.getContent())){
+                        sendResult(Protocol.CONNECTION_FAILURE);
+                    }
+                    else{
+                        sendResult(Protocol.CONNECTION_SUCCESS);
+                        nick = clientMessage.getContent();
+                        activeNicks.add(nick);
+                        clients.add(out);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            ResultMessage resMes;
+            while (sock.isConnected()) {
+                try{
+                    AbstractClientMessage mes = (AbstractClientMessage)(in.readObject());
+                    sendResult(Protocol.SENDING_MESSAGE_SUCCESS);
+                    ChatMessage chatMessage = new ChatMessage(mes.getContent(), nick, GregorianCalendar.getInstance().getTime());
+                    for(ObjectOutputStream o : clients) {
+                        o.writeObject(chatMessage);
+                    }
+                }
+                catch (Exception e) {
+                    sendResult(Protocol.SENDING_MESSAGE_FAILURE);
+                }
             }
         }
     }
