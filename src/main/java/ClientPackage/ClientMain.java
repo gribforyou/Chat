206cb6b26c9b58769ba0a28dfbccc04086575e1f
Dashuuka <sidorskaya.darya@gmail.com>
2024-11-14 00:16:06 +0300

    arguments

--- src/main/java/ClientPackage/ClientMain.java
+++ src/main/java/ClientPackage/ClientMain.java
@@ -2,9 +2,21 @@ package ClientPackage;
 
 public class ClientMain {
     public static void main(String[] args) {
-        String serverIp = "127.0.0.1";
+        String serverIp = "localhost";
         int port = 8071;
 
+        if (args.length >= 1) {
+            serverIp = args[0];  
+        }
+        if (args.length >= 2) {
+            try {
+                port = Integer.parseInt(args[1]);  
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid port number. Using default port 8071.");
+            }
+        }
+
+        System.out.println("Connecting to server at " + serverIp + ":" + port);
         Client client = new Client(serverIp, port);
         client.start();
     }
